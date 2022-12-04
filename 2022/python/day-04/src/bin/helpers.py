0c6b60d3ee5826cbb19d5a365a2efcd906fcfff8
leifeggenfellner <eggenfellner@protonmail.com>
2022-12-04 17:55:45 +0100

    refactor(pair1, pair2): simplify pair creation

--- 2022/python/day-04/src/bin/helpers.py
+++ 2022/python/day-04/src/bin/helpers.py
@@ -3,8 +3,7 @@ def process_part1(pairs: str) -> int:
     total = 0
 
     for line in lines:
-        pair1 = line[0].split("-")
-        pair2 = line[1].split("-")
+        pair1, pair2 = zip(line[0].split("-"), line[1].split("-"))
         pair1Numbers = set(range(int(pair1[0]), int(pair1[1]) + 1))
         pair2Numbers = set(range(int(pair2[0]), int(pair2[1]) + 1))
 
@@ -19,8 +18,7 @@ def process_part2(pairs: str) -> int:
     total = 0
 
     for line in lines:
-        pair1 = line[0].split("-")
-        pair2 = line[1].split("-")
+        pair1, pair2 = zip(line[0].split("-"), line[1].split("-"))
         pair1Numbers = set(range(int(pair1[0]), int(pair1[1]) + 1))
         pair2Numbers = set(range(int(pair2[0]), int(pair2[1]) + 1))
 
