27ae804ef1e1eb4dd4ac7f2b7cbd10ab323b3225
leifeggenfellner <eggenfellner@protonmail.com>
2022-12-23 16:35:15 +0100

    feat(day-12): complete day-12 tasks

--- 2022/python/day-12/src/bin/helpers.py
+++ 2022/python/day-12/src/bin/helpers.py
@@ -1,6 +1,70 @@
 def process_part1(data: str) -> int:
-    return
+    grid = [list(row) for row in data.splitlines()]
+
+    for row, line in enumerate(grid):
+        for column, letter in enumerate(line):
+            if letter == "S":
+                start_row = row
+                start_column = column
+                grid[row][column] = "a"
+            elif letter == "E":
+                end_row = row
+                end_column = column
+                grid[row][column] = "z"
+
+    queue = []
+    queue.append((0, start_row, start_column))
+
+    visited = {(start_row, start_column)}
+
+    while queue:
+        distance, row, column = queue.pop(0)
+        for next_row, next_column in [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]:
+            if next_row < 0 or next_row >= len(grid) or next_column < 0 or next_column >= len(grid[0]):
+                continue
+            elif (next_row, next_column) in visited:
+                continue
+            elif ord(grid[next_row][next_column]) - ord(grid[row][column]) > 1:
+                continue
+            elif next_row == end_row and next_column == end_column:
+                return distance + 1
+
+            visited.add((next_row, next_column))
+            queue.append((distance + 1, next_row, next_column))
+
+    return -1
 
 
 def process_part2(data: str) -> int:
-    return
+    grid = [list(row) for row in data.splitlines()]
+
+    for row, line in enumerate(grid):
+        for column, letter in enumerate(line):
+            if letter == "S":
+                grid[row][column] = "a"
+            elif letter == "E":
+                end_row = row
+                end_column = column
+                grid[row][column] = "z"
+
+    queue = []
+    queue.append((0, end_row, end_column))
+
+    visited = {(end_row, end_column)}
+
+    while queue:
+        distance, row, column = queue.pop(0)
+        for next_row, next_column in [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]:
+            if next_row < 0 or next_row >= len(grid) or next_column < 0 or next_column >= len(grid[0]):
+                continue
+            elif (next_row, next_column) in visited:
+                continue
+            elif ord(grid[next_row][next_column]) - ord(grid[row][column]) < -1:
+                continue
+            elif grid[next_row][next_column] == "a":
+                return distance + 1
+
+            visited.add((next_row, next_column))
+            queue.append((distance + 1, next_row, next_column))
+
+    return -1
--- 2022/python/day-12/test_parts.py
+++ 2022/python/day-12/test_parts.py
@@ -1,13 +1,15 @@
 from src.bin.helpers import process_part1, process_part2
-import pytest
 
-INPUT = """"""
+INPUT = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
 
 
 def test_part1():
-    assert process_part1(INPUT) == 13140
+    assert process_part1(INPUT) == 31
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_part2():
-    assert process_part2(INPUT) == 0
+    assert process_part2(INPUT) == 29
