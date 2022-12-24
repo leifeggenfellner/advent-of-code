3d10bc794d55d9672f21c537d9a533d251bf1af7
leifeggenfellner <eggenfellner@protonmail.com>
2022-12-24 16:27:07 +0100

    feat(day-14): complete day 14 tasks

--- 2022/python/day-14/src/bin/helpers.py
+++ 2022/python/day-14/src/bin/helpers.py
@@ -1,6 +1,80 @@
 def process_part1(data: str) -> int:
-    return
+    blocked = set()
+    abyss = 0
+
+    for line in data.splitlines():
+        coordinates = [list(map(int, pair.split(",")))
+                       for pair in line.split(" -> ")]
+
+        for (col1, row1), (col2, row2) in zip(coordinates, coordinates[1:]):
+            col1, col2 = sorted([col1, col2])
+            row1, row2 = sorted([row1, row2])
+
+            for col in range(col1, col2 + 1):
+                for row in range(row1, row2 + 1):
+                    blocked.add(col + row * 1j)
+                    abyss = max(abyss, row + 1)
+
+    total = 0
+
+    while True:
+        start = 500
+
+        while True:
+            if start.imag >= abyss:
+                return total
+            if start + 1j not in blocked:
+                start += 1j
+                continue
+            elif start + 1j - 1 not in blocked:
+                start += 1j - 1
+                continue
+            elif start + 1j + 1 not in blocked:
+                start += 1j + 1
+                continue
+
+            blocked.add(start)
+            total += 1
+            break
 
 
 def process_part2(data: str) -> int:
-    return
+    blocked = set()
+    floor = 0
+
+    for line in data.splitlines():
+        coordinates = [list(map(int, pair.split(",")))
+                       for pair in line.split(" -> ")]
+
+        for (col1, row1), (col2, row2) in zip(coordinates, coordinates[1:]):
+            col1, col2 = sorted([col1, col2])
+            row1, row2 = sorted([row1, row2])
+
+            for col in range(col1, col2 + 1):
+                for row in range(row1, row2 + 1):
+                    blocked.add(col + row * 1j)
+                    floor = max(floor, row + 1)
+
+    total = 0
+
+    while 500 not in blocked:
+        start = 500
+
+        while True:
+            if start.imag >= floor:
+                break
+            if start + 1j not in blocked:
+                start += 1j
+                continue
+            elif start + 1j - 1 not in blocked:
+                start += 1j - 1
+                continue
+            elif start + 1j + 1 not in blocked:
+                start += 1j + 1
+                continue
+            break
+
+        blocked.add(start)
+        total += 1
+
+    return total
--- 2022/python/day-14/test_parts.py
+++ 2022/python/day-14/test_parts.py
@@ -1,13 +1,12 @@
 from src.bin.helpers import process_part1, process_part2
-import pytest
 
-INPUT = """"""
+INPUT = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"""
 
 
 def test_part1():
-    assert process_part1(INPUT) == 13140
+    assert process_part1(INPUT) == 24
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_part2():
-    assert process_part2(INPUT) == 0
+    assert process_part2(INPUT) == 93
