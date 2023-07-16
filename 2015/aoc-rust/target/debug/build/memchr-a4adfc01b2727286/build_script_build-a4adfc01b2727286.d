5c491b575645e65e33b85fb7a1eebaae4af744af
leifeggenfellner <eggenfellner@protonmail.com>
2023-07-16 21:26:09 +0200

    feat: add gitignore

--- 2015/aoc-rust/target/debug/build/memchr-a4adfc01b2727286/build_script_build-a4adfc01b2727286.d
+++
@@ -1,5 +0,0 @@
-/Users/leif/Code/src/github/leifeggenfellner/advent-of-code/2015/aoc-rust/target/debug/build/memchr-a4adfc01b2727286/build_script_build-a4adfc01b2727286: /Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/Users/leif/Code/src/github/leifeggenfellner/advent-of-code/2015/aoc-rust/target/debug/build/memchr-a4adfc01b2727286/build_script_build-a4adfc01b2727286.d: /Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
--- 2015/aoc-rust/target/debug/deps/md5-101fbe7345210ca9.d
+++
@@ -1,7 +0,0 @@
-/Users/leif/Code/src/github/leifeggenfellner/advent-of-code/2015/aoc-rust/target/debug/deps/md5-101fbe7345210ca9.rmeta: /Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/md5-0.7.0/src/lib.rs
-
-/Users/leif/Code/src/github/leifeggenfellner/advent-of-code/2015/aoc-rust/target/debug/deps/libmd5-101fbe7345210ca9.rlib: /Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/md5-0.7.0/src/lib.rs
-
-/Users/leif/Code/src/github/leifeggenfellner/advent-of-code/2015/aoc-rust/target/debug/deps/md5-101fbe7345210ca9.d: /Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/md5-0.7.0/src/lib.rs
-
-/Users/leif/.cargo/registry/src/github.com-1ecc6299db9ec823/md5-0.7.0/src/lib.rs:
